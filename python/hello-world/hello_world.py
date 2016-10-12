6f3eccb5613c678086599594367a69cb954195f3
elaichi <hectorvd+github@gmail.com>
2016-10-11 23:03:10 -0400

    [solved] hello world

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,10 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+#
+
+
+def hello(name=''):
+    if name:
+        return u'Hello, {}!'.format(name)
+    else:
+        return u'Hello, World!'
